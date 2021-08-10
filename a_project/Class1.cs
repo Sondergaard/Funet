77139c038a435652c49aa15c9a1a27f77c91319d
Søren Søndergaard <s.sondergaard@icloud.com>
2021-08-10 15:18:10 +0200

    Add copy of projects

---
+++ a_project/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace a_project
+{
+    public class Class1
+    {
+    }
+}
---
+++ b_project/IProvide.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace b_project
+{
+    public interface IProvide
+    {
+    }
+}
